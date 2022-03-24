18a8623bd02f98229cb94ea8807c7cb4600641f3
Arthur-codeX <daniel.karanja239@gmail.com>
2022-03-25 02:22:03 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let sub = document.getElementById("submit");
+  let inp = document.getElementById("new-task-description");
+
+  sub.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (inp.value === "") {
+      return;
+    }
+    AddTask(document.querySelector("ul"), inp.value);
+    inp.value = "";
+  });
 });
+
+function AddTask(parent, text) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.innerText = "X";
+  btn.addEventListener("click", (e) => {
+    e.target.parentNode.remove();
+  });
+  li.innerText = text + " ";
+  li.appendChild(btn);
+  parent.appendChild(li);
+}
